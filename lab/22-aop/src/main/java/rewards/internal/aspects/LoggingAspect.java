287069167af577c1ada35ad28cfc124149f6ab9a
Alexander Fridman <alexnail@gmail.com>
2025-02-13 21:35:59 +0000

    Module 6 Lab completed

--- lab/22-aop/src/main/java/rewards/internal/aspects/LoggingAspect.java
+++ lab/22-aop/src/main/java/rewards/internal/aspects/LoggingAspect.java
@@ -3,9 +3,12 @@ package rewards.internal.aspects;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.stereotype.Component;
 import rewards.internal.monitor.Monitor;
 import rewards.internal.monitor.MonitorFactory;
 
@@ -16,13 +19,14 @@ import rewards.internal.monitor.MonitorFactory;
 //	- Optionally place @Autowired annotation on the constructor
 //    where `MonitorFactory` dependency is being injected.
 //    (It is optional since there is only a single constructor in the class.)
-
+@Aspect
+@Component
 public class LoggingAspect {
     public final static String BEFORE = "'Before'";
     public final static String AROUND = "'Around'";
 
-	private Logger logger = LoggerFactory.getLogger(getClass());
-	private MonitorFactory monitorFactory;
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	private final MonitorFactory monitorFactory;
 
 	
 	public LoggingAspect(MonitorFactory monitorFactory) {
@@ -30,12 +34,11 @@ public class LoggingAspect {
 		this.monitorFactory = monitorFactory;
 	}
 
-
 	// TODO-03: Write Pointcut Expression
 	// - Decide which advice type is most appropriate
 	// - Write a pointcut expression that selects only find* methods on
 	//    our repository classes.
-
+	@Before("execution(* rewards.internal.*.*Repository.find*(*))")
 	public void implLogging(JoinPoint joinPoint) {
 		// Do not modify this log message or the test will fail
 		logger.info(BEFORE + " advice implementation - " + joinPoint.getTarget().getClass() + //
@@ -48,7 +51,7 @@ public class LoggingAspect {
     // - Mark this method as an around advice.
 	// - Write a pointcut expression to match on all update* methods
 	//	 on all Repository classes.
-
+	@Around("execution(* rewards.internal.*.*Repository.update*(..))")
 	public Object monitor(ProceedingJoinPoint repositoryMethod) throws Throwable {
 		String name = createJoinPointTraceName(repositoryMethod);
 		Monitor monitor = monitorFactory.start(name);
@@ -58,8 +61,7 @@ public class LoggingAspect {
 			//  TODO-08: Add the logic to proceed with the target method invocation.
 			//  - Be sure to return the target method's return value to the caller
 			//    and delete the line below.
-
-			return new String("Delete this line after completing TODO-08");
+			return repositoryMethod.proceed();
 
 		} finally {
 			monitor.stop();
--- lab/22-aop/src/test/java/rewards/SystemTestConfig.java
+++ lab/22-aop/src/test/java/rewards/SystemTestConfig.java
@@ -1,13 +1,13 @@
 package rewards;
 
-import javax.sql.DataSource;
-
+import config.AspectsConfig;
+import config.RewardsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
-import config.RewardsConfig;
+import javax.sql.DataSource;
 
 
 /** 
@@ -16,10 +16,9 @@ import config.RewardsConfig;
  * and you should see one line of LoggingAspect output in the console.	 
  */
 @Configuration
-@Import({RewardsConfig.class})
+@Import({RewardsConfig.class, AspectsConfig.class})
 public class SystemTestConfig {
 
-	
 	/**
 	 * Creates an in-memory "rewards" database populated 
 	 * with test data for fast testing
