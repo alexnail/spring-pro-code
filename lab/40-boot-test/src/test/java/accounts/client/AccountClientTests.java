ca9ce5a56f84ebf23803a67de0018bd622c8814f
Alexander Fridman <alexnail@gmail.com>
2025-03-01 13:57:37 +0000

    Spring Boot Module 6 Lab completed

--- lab/40-boot-test/src/test/java/accounts/client/AccountClientTests.java
+++ lab/40-boot-test/src/test/java/accounts/client/AccountClientTests.java
@@ -2,17 +2,17 @@ package accounts.client;
 
 import common.money.Percentage;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 import rewards.internal.account.Account;
 import rewards.internal.account.Beneficiary;
 
 import java.net.URI;
 import java.util.Random;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 // TODO-00: In this lab, you are going to exercise the following:
 // - Using @SpringBootTest and webEnvironment for end-to-end testing
@@ -27,10 +27,13 @@ import static org.junit.jupiter.api.Assertions.*;
 // TODO-01: Make this class a Spring Boot test class
 // - Add @SpringBootTest annotation with WebEnvironment.RANDOM_PORT
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AccountClientTests {
 
 	// TODO-02: Autowire TestRestTemplate bean to a field
 	// - Name the field as restTemplate
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	// TODO-03: Update code below to use TestRestTemplate (as opposed to RestTemplate)
 	// - Remove RestTemplate from this code
@@ -44,16 +47,15 @@ public class AccountClientTests {
 	 * server URL ending with the servlet mapping on which the application can be
 	 * reached.
 	 */
-	private static final String BASE_URL = "http://localhost:8080";
+	//private static final String BASE_URL = "http://localhost:8080";
+	private static final String BASE_URL = "";
 
-	private RestTemplate restTemplate = new RestTemplate();
-	private Random random = new Random();
+	private final Random random = new Random();
 
 	@Test
 	public void listAccounts() {
 		String url = BASE_URL + "/accounts";
-		// we have to use Account[] instead of List<Account>, or Jackson won't know what
-		// type to unmarshal to
+		// we have to use Account[] instead of List<Account>, or Jackson won't know what type to unmarshal to
 		Account[] accounts = restTemplate.getForObject(url, Account[].class);
 		assertThat(accounts.length >= 21).isTrue();
 		assertThat(accounts[0].getName()).isEqualTo("Keith and Keri Donald");
@@ -92,10 +94,8 @@ public class AccountClientTests {
 	// TODO-04: Modify the code below so that it handles 404 HTTP response status
 	//          from the server (instead of handling it as an exception as in the
 	//          case of RestTemplate)
-	// - Remove the "assertThrows" statement (since you are not going to
-	//   check if an exception is thrown)
-	// - Use "getForEntity" method (instead of "getForObject" method) of
-	//   "TestRestTemplate"
+	// - Remove the "assertThrows" statement (since you are not going to check if an exception is thrown)
+	// - Use "getForEntity" method (instead of "getForObject" method) of "TestRestTemplate"
 	// - Verify that the HTTP response status is 404
 	// - Run all tests - they should all pass
 	@Test
@@ -108,17 +108,13 @@ public class AccountClientTests {
 
 		restTemplate.delete(newBeneficiaryLocation);
 
-		HttpClientErrorException httpClientErrorException = assertThrows(HttpClientErrorException.class, () -> {
-			System.out.println("You SHOULD get the exception \"No such beneficiary with name 'David'\" in the server.");
-			restTemplate.getForObject(newBeneficiaryLocation, Beneficiary.class);
-		});
-		assertThat(httpClientErrorException.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		var entity = restTemplate.getForEntity(newBeneficiaryLocation, Beneficiary.class);
+		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
 	// TODO-05: Observe a log message in the console indicating
 	//          Tomcat started as part of testing
 	// - Search for "Tomcat started on port(s):"
-	// - Note how long it takes for this test to finish - it is
-	//   in the range of several seconds
+	// - Note how long it takes for this test to finish - it is in the range of several seconds
 
 }
