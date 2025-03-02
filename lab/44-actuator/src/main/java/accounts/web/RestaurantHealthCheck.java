ed48558e34c3cd17d6e43d7ce5f5b875e458f766
Alexander Fridman <alexnail@gmail.com>
2025-03-02 17:52:10 +0000

    Spring Boot Module 8 Main Lab completed

--- lab/44-actuator/src/main/java/accounts/web/RestaurantHealthCheck.java
+++ lab/44-actuator/src/main/java/accounts/web/RestaurantHealthCheck.java
@@ -1,5 +1,11 @@
 package accounts.web;
 
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+import rewards.internal.restaurant.RestaurantRepository;
+
 /**
  * TODO-16a: Create custom health indicator
  * - Make this class implement HealthIndicator interface
@@ -9,8 +15,25 @@ package accounts.web;
  *   (no restaurants) or UP otherwise. (Note that RestaurantRepository
  *   has a method that returns number of restaurants.)
  */
-public class RestaurantHealthCheck {
+@Component
+public class RestaurantHealthCheck implements HealthIndicator {
 
+    private final RestaurantRepository repository;
+
+    public RestaurantHealthCheck(RestaurantRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Health health() {
+        var restaurantCount = repository.getRestaurantCount();
+        if (restaurantCount > 0) {
+            return Health.up().build();
+        } else {
+            //return Health.down().withDetail("restaurantCount", restaurantCount).build();
+            return Health.status(new Status("NO_RESTAURANTS")).build();
+        }
+    }
 }
 
 
